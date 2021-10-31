49b6a75438eae746c15aa935886213a10ae563f6
JC <jcaanacio@gmail.com>
2021-10-31 20:05:23 +0800

    build(transpiler): add build to git ignore

--- build/src/core/interfaces/encryption.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TokenPayloadPurpose = void 0;
-var TokenPayloadPurpose;
-(function (TokenPayloadPurpose) {
-    TokenPayloadPurpose["REGISTER"] = "REGISTER";
-    TokenPayloadPurpose["SIGN_IN"] = "SIGN_IN";
-})(TokenPayloadPurpose = exports.TokenPayloadPurpose || (exports.TokenPayloadPurpose = {}));
-//# sourceMappingURL=encryption.js.map
--- build/src/core/interfaces/entities/user.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserRole = void 0;
-var UserRole;
-(function (UserRole) {
-    UserRole["ADMIN"] = "ADMIN";
-    UserRole["USER"] = "USER";
-})(UserRole = exports.UserRole || (exports.UserRole = {}));
-//# sourceMappingURL=user.js.map
